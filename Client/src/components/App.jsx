4bbb97e6a4b1eba39a97e6bb32ad13cc2610bd67
Marlon Munoz <marlon@marlonshade.com>
2024-12-22 02:18:24 -0500

    mobile compatibility, dark-light mode feature add

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link }  from 'react-router-dom';
 import Transactions from '../components/Transactions';
 import AddTransactions from '../components/AddTransactions';
@@ -7,21 +7,39 @@ import Budget from '../components/Budget'
 import '/src/App.css'
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    if (!darkMode) {
+      document.body.classList.add('dark-mode');
+      document.body.classList.remove('light-mode');
+    } else {
+      document.body.classList.add('light-mode');
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
   return (
-    // <>
-    //   <h1>X-PENSE</h1>
-    //   <p>Seemlessly Track Your Budget!</p>
-    // </>
     <Router>
-      <div className='container'>
+      <div className={`container d-flex flex-column align-items-center ${darkMode ? 'dark-mode' : 'light-mode'}`}>
         <h1>Budget Tracker</h1>
-        <nav>
-          <ul className='nav nav-tabs justify-content-center'>
-            <li className='nav-item'><Link className='nav-link' to='/'>Home</Link></li>
-            <li className='nav-item'><Link className='nav-link' to='/transactions'>Transactions</Link></li>
-            <li className='nav-item'><Link className='nav-link' to='/add'>Add Transaction</Link></li>
-            <li className='nav-item'><Link className='nav-link' to='budget'>Budget</Link></li>
-          </ul>
+        <button onClick={toggleDarkMode} className='btn btn-secondary mb-3'>
+          Toggle {darkMode ? 'Light': 'Dark'} Mode
+        </button>
+        
+        <nav className={`navbar navbar-expand-lg ${darkMode ? 'navbar-dark-mode' : 'navbar-light-mode'}`}>
+          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toogle navigation'>
+            <span className='navbar-toggler-icon'></span>
+          </button>
+          <div className='collapse navbar-collapse justify-content-center' id='navbarNav'>
+            <ul className='nav nav-tabs'>
+              <li className='nav-item'><Link className= {`nav-link ${darkMode ? 'nav-link-dark-mode' : 'nav-link-light-mode'} border `} to='/'>Home</Link></li>
+              <li className='nav-item'><Link className= {`nav-link ${darkMode ? 'nav-link-dark-mode' : 'nav-link-light-mode'} border `} to='/transactions'>Transactions</Link></li>
+              <li className='nav-item'><Link className= {`nav-link ${darkMode ? 'nav-link-dark-mode' : 'nav-link-light-mode'} border `} to='/add'>Add Transaction</Link></li>
+              <li className='nav-item'><Link className= {`nav-link ${darkMode ? 'nav-link-dark-mode' : 'nav-link-light-mode'} border `} to='budget'>Budget</Link></li>
+            </ul>
+          </div>
         </nav>
         <Routes>
           <Route path='/' element ={<Balance />}/>
