cd5388730b3f3a10fe4c411e9b1d4519e944c85d
Marlon Munoz <marlon@marlonshade.com>
2024-12-29 06:39:42 -0500

    added progress bar in Goals.jsx component

--- Client/src/components/Goals.jsx
+++ Client/src/components/Goals.jsx
@@ -4,8 +4,18 @@ function Goals() {
     return (
         <div>
             <h5>Goals</h5>
+            <div className="mb-3">
+                <p>Irynkita's Savings Account</p>
+                <div className="progress">
+                    <div className="progress-bar" role="progressbar" style={{width: "10%"}} aria-valuemin="0" aria-valuemax="100">10%</div>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default Goals;
+
+{/* <p>New Home</p>
+<p>New Car</p>
+<p>Vacations</p> */}
--- Client/src/components/Investments.jsx
+++ Client/src/components/Investments.jsx
@@ -4,6 +4,8 @@ function Investments() {
     return (
         <div>
             <h5>Investments</h5>
+            <p>Stocks</p>
+            <p>Crypto</p>
         </div>
     )
 }
