f5c43c7fe0bf2f9481e0b4a057c04b7ad341af66
Marlon Munoz <marlon@marlonshade.com>
2024-12-29 00:04:21 -0500

    populated app.py to connect backend and frontend via FLASK

---
+++ Server/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app) # Enables CORS for all routes
+
+users = {
+    "user1:" "password1",
+    "user2:" "password2"
+}
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json 
+    username = data.get('username')
+    password  = data.get('password')
+    
+    if username in users and users[username] == password:
+        return jsonify({"message": "Login Successful"}), 200
+    else:
+        return jsonify({"message": "Invalid Credentials"}), 401
+if __name__ == '__main__':
+    app.run(debug=True)
