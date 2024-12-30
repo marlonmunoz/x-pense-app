af9963e1a96f98bf5397212c17bb0daee859341d
Marlon Munoz <marlon@marlonshade.com>
2024-12-30 00:20:09 -0500

    minor detailing style in Footer component

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <footer className="text-center my-5">
-            <p>&copy; {new Date().getFullYear()} MarlonShade. All rights reserved.</p>
+            <p>&copy;{new Date().getFullYear()} MarlonShade. All rights reserved.</p>
         </footer>
     )
 }
