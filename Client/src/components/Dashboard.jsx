3bd4f5494b5059ba2732570ba6dfaee74976a1ef
Marlon Munoz <marlon@marlonshade.com>
2025-01-03 16:17:04 -0500

    added resposive table for Dashboard component

--- Client/src/components/Dashboard.jsx
+++ Client/src/components/Dashboard.jsx
@@ -5,13 +5,14 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Responsi
 
 function Dashboard({ transactions =[], balance =0, goals =[], investments= [], budget = 0, totalAmount }) {
     const navigate = useNavigate();
-
+    
     const recentTransactions = transactions.slice(-5);
     const totalInvestments = investments.reduce((total, investment) => total + investment.amount, 0);
     const goalsProgress = goals.map(goal => ({
         ...goal,
         progress: (goal.saved / goal.target) * 100
     }));
+    const overviewTotal = budget + totalInvestments - balance - totalAmount;
 
     return (
         <div>
@@ -27,12 +28,39 @@ function Dashboard({ transactions =[], balance =0, goals =[], investments= [], b
                 <button onClick={() => navigate('/transactions')} className="btn btn-primary"> View All Transactions</button>
             </div>
             <br />
-            <div className="balance-overview">
+            {/* <div className="balance-overview">
                 <h5>Overview</h5>
                 <p>Current Balance: ${balance}</p>
                 <p>Total Investments: ${totalInvestments}</p>
                 <p>Budget: ${budget}</p>
                 <p>X-penses: ${totalAmount}</p>
+                <h6>Overview Total: ${overviewTotal}</h6>
+            </div> */}
+            <div className="table-responsive">
+                <table className="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Current Balance</th>
+                            <td>${balance}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Total Investments</th>
+                            <td>${totalInvestments}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Budget</th>
+                            <td>${budget}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">X-penses</th>
+                            <td>${totalAmount}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Overview Total</th>
+                            <td>${overviewTotal}</td>
+                        </tr>
+                    </tbody>
+                 </table>
             </div>
             <div className="goals-progress">
                 <h5>Goals Progress</h5>
@@ -46,7 +74,7 @@ function Dashboard({ transactions =[], balance =0, goals =[], investments= [], b
             </div>
             <div className="charts">
                 <h5>Spending Over Time</h5>
-                <ResponsiveContainer width="101%" height={300}>
+                <ResponsiveContainer width="100%" height={300}>
                     <LineChart data={transactions}>
                         <CartesianGrid strokeDasharray="3 3" />
                         <XAxis dataKey="date"/>
