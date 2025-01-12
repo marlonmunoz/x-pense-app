f73727baf5ae620ff590503fb3869aa7ae9b67ba
Marlon Munoz <marlon@marlonshade.com>
2025-01-12 10:26:21 -0500

    updated code for s key

--- Server/generate_secret_key.py
+++ Server/generate_secret_key.py
@@ -1,3 +1,10 @@
+import os
 import secrets
-secret_key = secrets.token_hex(32)
-print(secret_key)
+
+# Generate a new secret key if it doesn't exist
+if 'SECRET_KEY' not in os.environ:
+    secret_key = secrets.token_hex(32)
+    print(f"Generated secret key: {secret_key}")
+else:
+    secret_key = os.environ['SECRET_KEY']
+    print(f"Using existing secret key: {secret_key}")
