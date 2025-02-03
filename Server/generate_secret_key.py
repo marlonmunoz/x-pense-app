a85c515ba000fefd65e94306dd3b35603b92a741
Marlon Munoz <marlon@marlonshade.com>
2025-02-03 00:22:54 -0500

    new sk updated and new file added to .gitignore file

--- Server/generate_secret_key.py
+++
@@ -1,8 +0,0 @@
-import os
-import secrets
-
-# Generate a new secret key if it doesn't exist
-if 'SECRET_KEY' not in os.environ:
-    secret_key = secrets.token_hex(32)
-else:
-    secret_key = os.environ['SECRET_KEY']
