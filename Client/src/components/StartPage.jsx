7987bf004f0104cf052e35eb69b7411a05feb392
Marlon Munoz <marlon@marlonshade.com>
2025-02-22 19:01:55 -0500

    added effects snd sound to the StartPage component

--- Client/src/components/StartPage.jsx
+++ Client/src/components/StartPage.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import intro_01 from '../assets/sounds/intro_01.mp3'
 
 const StartPage = () => {
     const navigate = useNavigate();
 
     const handleStartClick = () => {
+        const audio = new Audio(intro_01);
+        audio.volume = 0.1;
+        audio.play().catch(error => {
+            console.error("Error playing audio", error)
+        })
         navigate('/dashboard');
     };
 
