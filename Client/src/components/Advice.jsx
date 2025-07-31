ad7b76f50fe31415364b7d7d48520faab24a9810
Marlon Munoz <marlon@marlonshade.com>
2025-08-01 01:44:29 +0200

    sidebar nav is not displying properly in mobile version, needs lots of debbuging

---
+++ Client/src/components/Advice.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Advice({ darkMode }) {
+    return (
+        <div>
+            <h1>ADVICE TO BE ADDED LATER</h1>
+        </div>
+    )
+}
+
+export default Advice;
